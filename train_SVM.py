e9fddf22c4ab28591abc94952a77e4ef9aef8106
Rashad Garayev <pythonaz@yahoo.com>
2020-03-11 18:24:50 +0400

    Classification-training

---
+++ train_SVM.py
@@ -0,0 +1,45 @@
+from skimage.feature import hog
+from skimage.io import imread
+import joblib,glob,os,cv2
+
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn import svm, metrics
+import numpy as np 
+from sklearn.preprocessing import LabelEncoder
+
+train_data = []
+train_labels = []
+pos_im_path = 'DATAIMAGE/positive/'
+neg_im_path = 'DATAIMAGE/negative/'
+model_path = 'models/model.dat'
+# Load the positive features
+for filename in glob.glob(os.path.join(pos_im_path,"*.png")):
+    fd = cv2.imread(filename,0)
+    fd = cv2.resize(fd,(64,128))
+    fd = hog(fd,orientations=9,pixels_per_cell=(8,8),visualize=False,cells_per_block=(3,3))
+    train_data.append(fd)
+    train_labels.append(1)
+
+# Load the negative features
+for filename in glob.glob(os.path.join(neg_im_path,"*.jpg")):
+    fd = cv2.imread(filename,0)
+    fd = cv2.resize(fd,(64,128))
+    fd = hog(fd,orientations=9,pixels_per_cell=(8,8),visualize=False,cells_per_block=(3,3))
+    train_data.append(fd)
+    train_labels.append(0)
+train_data = np.float32(train_data)
+train_labels = np.array(train_labels)
+print('Data Prepared........')
+print('Train Data:',len(train_data))
+print('Train Labels (1,0)',len(train_labels))
+print("""
+Classification with SVM
+
+""")
+
+model = LinearSVC()
+print('Training...... Support Vector Machine')
+model.fit(train_data,train_labels)
+joblib.dump(model, 'models/models.dat')
+print('Model saved : {}'.format('models/models.dat'))
