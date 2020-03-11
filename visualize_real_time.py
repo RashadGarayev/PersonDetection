c06b037094760a8f173744e011df75d581db4494
Rashad Garayev <pythonaz@yahoo.com>
2020-03-11 20:32:15 +0400

    Update visualize_real_time.py

--- visualize_real_time.py
+++ visualize_real_time.py
@@ -6,9 +6,6 @@ from skimage.feature import hog
 import pafy,joblib,cv2
 from skimage import color
 import Sliding as sd
-
-
-
 size = (64,128)
 step_size = (10,10)
 downscale = 1.25
@@ -45,26 +42,18 @@ while True:
                     detections.append((int(x * (downscale**scale)), int(y * (downscale**scale)), model.decision_function(fd), 
                     int(size[0] * (downscale**scale)),
                     int(size[1] * (downscale**scale))))
-    
         scale += 1
-
     clone = image.copy()
-
-    for (x_tl, y_tl, _, w, h) in detections:
-        cv2.rectangle(image, (x_tl, y_tl), (x_tl + w, y_tl + h), (0, 255, 0), thickness = 2)
-
     rects = np.array([[x, y, x + w, y + h] for (x, y, _, w, h) in detections])
     sc = [score[0] for (x, y, score, w, h) in detections]
-    
+    print('sc:',sc)
     sc = np.array(sc)
     pick = non_max_suppression(rects, probs = sc, overlapThresh = 0.3)
 
     for(x1, y1, x2, y2) in pick:
         cv2.rectangle(clone, (x1, y1), (x2, y2), (0, 255, 0), 2)
         cv2.putText(clone,'Person : {:.2f}'.format(np.max(sc)),(x1-2,y1-2),1,1,(0,122,12),1)
-    #cv2.imshow('webcam before nms',image)
     cv2.imshow('Person Detection',clone)
-    
     if cv2.waitKey(1)==27:
         break
 cap.release()
